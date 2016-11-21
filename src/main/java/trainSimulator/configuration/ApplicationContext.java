02d1afd7a0dfd344e56c975345f17e1f453bbda9
Wojciechbator <wojciech.jan.bator@gmail.com>
2016-11-21 12:28:03 +0100

    added beans definitions to the project, still hibernate mapping to fix, and view resolver somehow doesn't react properly

--- src/main/java/trainSimulator/configuration/ApplicationContext.java
+++ src/main/java/trainSimulator/configuration/ApplicationContext.java
@@ -1,5 +1,7 @@
 package trainSimulator.configuration;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import trainSimulator.repositories.*;
+import trainSimulator.repositories.implementations.*;
 
 /**
  * Created by mitron-wojtek on 15.11.16.
@@ -44,5 +48,53 @@ public class ApplicationContext extends WebMvcConfigurerAdapter {
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Autowired
+    @Bean(name = "eventLogDao")
+    public EventLogsDao getEventLogsDao(SessionFactory sessionFactory) {
+        return new EventLogsDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "generatorParametersDao")
+    public GeneratorParametersDao getGeneratorParametersDao(SessionFactory sessionFactory) {
+        return new GeneratorParametersDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "passengersDao")
+    public PassengersDao getPassengersDao(SessionFactory sessionFactory) {
+        return new PassengersDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "routesDao")
+    public RoutesDao getRoutesDao(SessionFactory sessionFactory) {
+        return new RoutesDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "stationsDao")
+    public StationsDao getStationsDao(SessionFactory sessionFactory) {
+        return new StationsDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "ticketsDao")
+    public TicketsDao getTicketsDao(SessionFactory sessionFactory) {
+        return new TicketsDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "timetableEntitiesDao")
+    public TimetableEntitiesDao getTimetableEntitiesDao(SessionFactory sessionFactory) {
+        return new TimetableEntitiesDaoImplementation(sessionFactory);
+    }
+
+    @Autowired
+    @Bean(name = "trainsDao")
+    public TrainsDao getTrainsDao(SessionFactory sessionFactory) {
+        return new TrainsDaoImplementation(sessionFactory);
     }
 }
